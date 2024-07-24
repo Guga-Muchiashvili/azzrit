24e40d17e38fe1a9b6df118580a54b3115addeee
guga-ggw <ggwomega15@gmail.com>
2024-07-24 15:00:11 +0400

    fix type errors

--- app/(protected)/components/PlayerList/PlayerListComponent.tsx
+++ app/(protected)/components/PlayerList/PlayerListComponent.tsx
@@ -9,23 +9,24 @@ import { IUser } from "@/types/types";
 import Image from "next/image";
 
 const PlayerListComponent = () => {
-  const [table, setTable] = useState<ITable | null>(null);
+    const [table, setTable] = useState<ITable | null>(null);
   const { tableId } = useParams();
-  const {getTableUsers} = useTypeContext()
-  const [players, setPlayers] = useState<IUser[]>([])
-
+  const { getTableUsers } = useTypeContext();
+  const [players, setPlayers] = useState<IUser[]>([]);
 
   useEffect(() => {
     const getTable = async () => {
-      const table = await getTableById(tableId as string);
-      if (table) {
-        setTable(table as any);
+      const fetchedTable = await getTableById(tableId as string);
+      if (fetchedTable) {
+        setTable(fetchedTable as any);
+        if ((fetchedTable as any).id) {
+          const fetchedPlayers = await getTableUsers((fetchedTable as any).id);
+          setPlayers(fetchedPlayers as any);
+        }
       }
-      const ml = await getTableUsers(table?.id);
-      setPlayers(ml as any)
     };
     getTable();
-  }, [tableId]);
+  }, [tableId, getTableUsers]);
   
 
   
@@ -34,7 +35,7 @@ const PlayerListComponent = () => {
       <CretorControlComponent id={table?.creatorId} />
       <div className="w-full h-full flex flex-wrap p-20 gap-12">
       {players.map((item) => (
-        <div className="w-96 h-80 bg-gray-500 rounded-xl relative flex">
+        <div key={item.id} className="w-96 h-80 bg-gray-500 rounded-xl relative flex">
             <div className="w-full h-14 flex gap-2 px-5 items-start py-3">
             <Image src={item.image as string} width={1200} height={1200} alt="picture" className="rounded-full h-12 w-12" />
             </div>
