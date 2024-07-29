6ae1af97fea6d610b47e8cf22f3c041341c34ae6
guga-ggw <ggwomega15@gmail.com>
2024-07-29 15:04:49 +0400

    add socket server

--- app/(protected)/components/gamePageComponent/GamePage.tsx
+++ app/(protected)/components/gamePageComponent/GamePage.tsx
@@ -9,19 +9,56 @@ import { useTypeContext } from '../../tableTypeContext/TypeContext'
 import TableCardElement from '../tableComponent/TableCard'
 import FilterTableComponent from '../filterComponent/FilterTable'
 import { Toaster } from 'sonner'
+import { socket } from '@/socket'
 const GamePageComponent = () => {
   const { defineType, type, Tables, fetchData } = useTypeContext()
   const session = useSession()
+  const [isConnected, setIsConnected] = useState(false);
+  const [transport, setTransport] = useState("N/A");
 
   useEffect(() => {
+    if (socket.connected) {
+      onConnect();
+    }
+
+    function onConnect() {
+      setIsConnected(true);
+      setTransport(socket.io.engine.transport.name);
+
+      socket.io.engine.on("upgrade", (transport) => {
+        setTransport(transport.name);
+      });
+    }
+
+    function onDisconnect() {
+      setIsConnected(false);
+      setTransport("N/A");
+    }
+
+    socket.on("connect", onConnect);
+    socket.on("disconnect", onDisconnect);
+
+    socket.on('tables', (newTables) => {
+      console.log('Received tables:', newTables);
+    });
+
     fetchData()
       const deleteTableId = async() => {
         const res = await deleteUserTableId(session.data?.user.id as string)
         console.log(res)
       }
       deleteTableId()
+      socket.on('tables', (value) => {
+        console.log('tab', value)
+      })
 
+      return () => {
+        socket.off("connect", onConnect);
+        socket.off("disconnect", onDisconnect);
+      }
   },[])
+
+  console.log(isConnected)
 
   return (
     <div className="w-full min-h-screen relative flex flex-col items-center py-9" id="MainPage">
